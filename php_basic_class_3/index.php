62ad213877a7fe03005eba2e33df4628dad1dafe
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-06-26 11:01:44 +0600

    class 2

--- php_basic_class_3/index.php
+++ php_basic_class_3/index.php
@@ -1,128 +1,131 @@
 <?php
-/*==========================
-    PHP Arithmetic Operators
-   ==========================*/
-// +, -, *, /, %
-
-// $number = 20;
-// $number += 10;
-// echo $number;
-
-// assign
-// $number = 20;
-// $number2 = $number + 10;
-// echo $number, "</br>" ,$number2;
-
-// $number =  30;
-// $number2 = $number - 20;
-// echo $number, "</br>", $number2;
-
-// $number = 20;
-// $number -= 10;
-// echo $number;
-
-// $number = 20;
-// $number *= 10;
-// echo $number;
-
-// $number = 12; 
-// $number = $number % 3;
-// echo $number;
+/*====================
+    printf
+=====================*/
+// $firstName = 'Mr.';
+// $lastName = 'Hossain';
+// printf('My name is %s %s', $firstName, $lastName);
+// printf('My name is %2$s %1$s', $firstName, $lastName);
+//sprintf
+// $name = sprintf('My name is %s %s', $firstName, $lastName);
+// echo $name;
 
 
 
-/*==========================
-    post-increment
-   ==========================*/
+/*====================
+    php if statement
+=====================*/
 
-// echo 'post-increment:', PHP_EOL;
-// $a = 5;
-// var_dump($a++);
-// var_dump($a);
+// $a = 70;
+// $b = 60;
 
-// echo 'post-increment:',PHP_EOL;
-// $num = 10;
-// var_dump($num++);
-// var_dump($num);
-
-
-/*==========================
-    pre-increment
-   ==========================*/
-// echo 'pre-increment:', PHP_EOL;
-// $num = 20;
-// var_dump(++$num);
-// var_dump($num);
-
-// echo 'pre-increment:', PHP_EOL;
-// $num = 10;
-// var_dump(++$num);
-// var_dump($num);
-
-
-/*==========================
-    PHP for Loop
-   ==========================*/
-
-// echo 'Alphabatic string:'. PHP_EOL;
-// $s = 'A';
-// for ($i=0; $i<6; $i++){
-//     echo $s++ . PHP_EOL;
+// if($a > $b) {
+//     echo 'a is gretar than b';
 // }
 
-// $num = 20;
-// for($n=0; $n<10; $n++){
-//     echo $num++;
-// }
-
-// echo (pi()."</br>");
-// echo (max(20, 10, 30, 50, 100, -8, -200) ."</br>");
-// echo (min(20, 10, 30, 50, 100, -8, -200) ."</br>");
-// echo (abs(-8.6));
-// echo (round(-8.4));
-// echo rand();
-// echo rand(1, 10);
-
-
-/* ===================
-    and operator
- =================== */
-// $x = 100;
-// $y = 50;
-// if($x==100 && $y==50){
-//     echo 'Hello world!';
-// }
-
-
-/* ===================
-    or operator
- =================== */
-// $x = 100;
-// $z = 40;
-// if($x == 100 || $z = 50){
-//     echo "Hello world !";
-// }
-
-/* ===================
-    not operator
- =================== */
-// $x = 100;
-// if($x !== 100){
-//     echo "Yes";
+// if (false) {
+//     echo 'true';
 // } else {
-//     echo "No";
+//     echo 'false';
 // }
 
-// $text = 'hello';
-// $text1 = 'world';
-// $text .= $text1;
-// echo $text;
+// $a = 50;
+// $b = 60;
+
+// if($a > $b) {
+//     echo 'a grater than b';
+// } elseif ($a === $b) {
+//     echo 'a equal to b';
+// } elseif ($a != $b) {
+//     echo 'a not equal to b';
+// } else {
+//     echo 'a is greater than b and is not equal to b';
+// }
 
 
-/* ===================
-    array
- =================== */
+/*====================
+    for wordpress
+=====================*/
 
-$x = ['a' => 'red' , 'b' => 'green'];
-$z = ['a' => 'blue' , 'b' => 'yellow'];
-var_dump($x === $z);
+// if($a > $b) {
+//     echo 'a grater than b';
+// } elseif ($a === $b) {
+//     echo 'a equal to b';
+// } elseif ($a != $b) {
+//     echo 'a not equal to b';
+// } else {
+//     echo 'a is greater than b and is not equal to b';
+// }
+
+// $a = 5;
+// if($a == 5):
+//     echo "a equals 5";
+//     elseif ($a == 6):
+//         echo "a equals 6";
+//     else:
+//         echo 'a is naither 5 or 6';
+//     endif;
+
+// $b = 6;
+//     if($b == 6):
+//         echo "a equals 6";
+//     elseif ($b == 7):
+//         echo "a equals 7";
+//     else:
+//         echo "a is naither 6 or 7";
+//     endif;
+
+// $num = 10;
+// if($num == 10):
+//     echo "num equals 10";
+// elseif($num == 11):
+//     echo "num equals 11";
+// else:
+//     echo "num is naither 10 or 11";
+// endif;
+
+// variable scope
+//1. local, 
+/* local
+    function getLocal(){
+    echo 'hello world!';
+    }
+*/
+
+//2.global,
+$parsonName = "Hossain"; //global
+$parsonPhone = 56754333; //global
+
+function getNumber(){
+    global $parsonName;
+    echo "My name is $parsonName </br>";
+}
+getNumber();
+
+function getPnum(){
+echo $GLOBALS['parsonPhone'];
+}
+getPnum();
+
+// 3.static
+
+
+/*====================
+    function
+=====================*/
+//function
+//1. built in function
+//2. user define function
+
+//user define function
+// function getAddress() {
+//     echo 'Hello world';
+// }
+// getAddress();
+
+// $num = 455656544543;
+// function getAddress() {
+// $num = 455656544543;
+//     echo $num;
+// }
+// getAddress();
